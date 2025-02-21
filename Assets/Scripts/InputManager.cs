5771a05285ec228117dc1234a3b9e4fef43db233
RDArtemis <144180579+RDArtemis@users.noreply.github.com>
2025-02-20 19:13:35 -0800

    Update InputManager.cs

--- Assets/Scripts/InputManager.cs
+++ Assets/Scripts/InputManager.cs
@@ -1,16 +1,42 @@
 using UnityEngine;
+using UnityEngine.Events;
 
 public class InputManager : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
-    {
-        
-    }
+    public UnityEvent<Vector2> OnMove = new UnityEvent<Vector2>(); //move around
+    public UnityEvent OnJump = new UnityEvent(); //jump
+    public UnityEvent OnDash = new UnityEvent(); //dash 
 
-    // Update is called once per frame
+    
     void Update()
     {
+        //move direction controls
+        Vector2 inputVector = Vector2.zero;
+        if (Input.GetKey(KeyCode.W))
+        {
+            inputVector += Vector2.up;  //forward
+        }
+        if (Input.GetKey(KeyCode.S))
+        {
+            inputVector += Vector2.down;  //backward
+        }
+        if (Input.GetKey(KeyCode.A))
+        {
+            inputVector += Vector2.left;  //left
+        }
+        if (Input.GetKey(KeyCode.D))
+        {
+            inputVector += Vector2.right;  //right
+        }
+
+
+        OnMove?.Invoke(inputVector);
+
         
+        if (Input.GetKey(KeyCode.Space))
+        {
+            OnJump?.Invoke(); //jump time!
+        }
+
     }
 }
